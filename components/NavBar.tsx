20c1d1b305a3c8361e0393a72e01faecaac1e5db
Federica Ventura <federica.ventura@avanade.com>
2024-05-07 17:01:13 +0200

    Lesson 11

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
+import { FaGoogle } from 'react-icons/fa';
 
 const NavBar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
