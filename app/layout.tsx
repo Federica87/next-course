70e301d4e481e69d2aad128e61f28037c7520559
Federica Ventura <federica.ventura@avanade.com>
2024-05-14 09:47:37 +0200

    Lessons 12-13

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '@/components/NavBar';
 import '@/assets/styles/global.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
       <body>
         <NavBar />
         <main>{children}</main>;
+        <Footer />
       </body>
     </html>
   );
